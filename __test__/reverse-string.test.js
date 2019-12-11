5f59f70a7f9bdad09416d8139e6e985bfd9c10d0
imhta <imhtapm@gmail.com>
2019-12-11 19:44:50 +0530

    :white_check_mark: :heavy_plus_sign: add another test for testing with bad input

--- __test__/reverse-string.test.js
+++ __test__/reverse-string.test.js
@@ -5,3 +5,8 @@ it('reverse a word string', () => {
   const expectedOutput = 'emesrever';
   expect(input).toBe(expectedOutput);
 });
+
+it('bad input as number to reverse string', () => {
+  const input = reverseString(1);
+  expect(input).toThrow(new Error('Input is not string'));
+});
