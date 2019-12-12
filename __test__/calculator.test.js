6f3a0327b8190dcf4a5c4f1eaaaa1bbd33910900
imhta <imhtapm@gmail.com>
2019-12-12 17:20:12 +0530

    :white_check_mark: :hammer: add multiple casdate test and bad input test

--- __test__/calculator.test.js
+++ __test__/calculator.test.js
@@ -23,3 +23,20 @@ it('divide numbers', () => {
   const outputValue = calculator().divide(...arr).currentValue;
   expect(outputValue).toBe(2);
 });
+
+it('multiple cascade operation test', () => {
+  const outputValue = calculator()
+    .add(1, 2, 3)
+    .subtract(2)
+    .divide(2)
+    .add(2);
+  expect(outputValue.currentValue).toBe(4);
+});
+
+it('bad input as string to calculator', () => {
+  const badCall = () => {
+    calculator().add(1, 2, 'I am bad input');
+  };
+
+  expect(badCall).toThrow(new Error('BadInput: Only numbers are allowed as input'));
+});
